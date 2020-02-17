c85a37bd54aee6dd8c27f90c2c5a6475d32d55bf
Amnah Abdulal <as-1991-as@hotmail.com>
2020-02-17 22:12:14 +0300

    new requremint

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
 
 // image 1 event 
-let image1 = document.querySelector(".cat-img-1");
-let numOfClickes1 = 0; 
-let counter1 = document.querySelector(".counter-1");
-counter1.innerHTML = numOfClickes1
-image1.addEventListener('click' , () => {
-    console.log('image was clicked');
-    numOfClickes1 += 1;
-    counter1.innerHTML = numOfClickes1
-})
+// let image1 = document.querySelector(".cat-img-1");
+// let numOfClickes1 = 0; 
+// let counter1 = document.querySelector(".counter-1");
+// counter1.innerHTML = numOfClickes1
+// image1.addEventListener('click' , () => {
+//     console.log('image was clicked');
+//     numOfClickes1 += 1;
+//     counter1.innerHTML = numOfClickes1
+// })
 
 
-// image 2 event
+// 1 - creat list of 5 cat to display in cat gallery
 
-let image2 = document.querySelector(".cat-img-2");
-let numOfClickes2 = 0; 
-let counter2 = document.querySelector(".counter-2");
-counter2.innerHTML = numOfClickes2
-image2.addEventListener('click' , () => {
-    console.log('image was clicked');
-    numOfClickes2 += 1;
-    counter2.innerHTML = numOfClickes2
-})
+// a.
+let container = document.querySelector('.container');
+let div = document.createElement('div');
+div.className = 'cat-gallery';
+container.appendChild(div);
+// b.
+let img = [];
+for (var i = 1; i <= 5; i++) {
+    img[i] = document.createElement('img');
+    img[i].setAttribute('src', `/css/cat-${i}.jpg`);
+    img[i].className = 'cat-img';
+    div.appendChild(img[i]);
+}
+
+// 2 - add eventlistener to image gallery
+
+div.addEventListener('click', (e) => {
+    if (e.target.getAttribute('src')) {
+        let selectedImg = document.createElement('img');
+        selectedImg.setAttribute('src', `${e.target.getAttribute('src')}`);
+        selectedImg.className = 'selected-img';
+        container.appendChild(selectedImg);
+    }
+});
+
+// 3 - display the clicked cat in cat area 
+// 4 - add eventlistener to cat area that shows num of clickes 
